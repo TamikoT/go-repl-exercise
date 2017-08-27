095c551106cd3a917b5e2be9e8326bcece461d19
TamikoT <tamikodevo@gmail.com>
2017-08-26 23:22:35 -0700

    clean up comments and naming

--- main.go
+++ main.go
@@ -7,55 +7,57 @@ import (
 	"strings"
 )
 
-type Transaction struct {
-	parent *Transaction
+// transaction : represented as node of linked list
+type transaction struct {
+	parent *transaction
 	data   map[string]string
 }
 
-func Start(p *Transaction) *Transaction {
-	d := make(map[string]string)
-	t := &Transaction{parent: p, data: d}
-	if !(p == nil) {
-		for k, v := range p.data {
-			t.data[k] = v
+// start : creates  new transaction
+func start(parent *transaction) *transaction {
+	newData := make(map[string]string)
+	head := &transaction{parent: parent, data: newData}
+	// if transaction is not the tail,copy data
+	if !(parent == nil) {
+		for k, v := range parent.data {
+			head.data[k] = v
 		}
 	}
-	return t
-}
-
-// returns true if current node is head
-func isHead(t *Transaction) bool {
-	if t.parent == nil {
-		return true
-	} else {
-		return false
-	}
-}
-
-// set head to previous
-func Abort(head *Transaction) *Transaction {
-	return head.parent
-}
-
-// delete previous node + current points to parent's parent
-func Commit(head *Transaction) *Transaction {
-	grandparent := head.parent.parent
-	head.parent = grandparent
 	return head
 }
 
-func (n *Transaction) Write(k, v string) {
-	n.data[k] = v
+// abort : sets head to previous transaction
+func abort(head *transaction) *transaction {
+	return head.parent
 }
 
-func (t *Transaction) Read(k string) (string, error) {
+// commit : deletes parent + points current node to parent's parent
+func commit(head *transaction) *transaction {
+	prevParent := head.parent.parent
+	head.parent = prevParent
+	return head
+}
+
+// isTail : checks if current node is tail
+func isTail(current *transaction) bool {
+	if current.parent == nil {
+		return true
+	}
+	return false
+}
+
+func (t *transaction) write(k, v string) {
+	t.data[k] = v
+}
+
+func (t *transaction) read(k string) (string, error) {
 	if v, ok := t.data[k]; ok {
 		return v, nil
 	}
 	return "", fmt.Errorf("Key not found: %s", k)
 }
 
-func (t *Transaction) Delete(k string) error {
+func (t *transaction) delete(k string) error {
 	if _, ok := t.data[k]; ok {
 		delete(t.data, k)
 		fmt.Println("Key deleted: " + k)
@@ -65,47 +67,44 @@ func (t *Transaction) Delete(k string) error {
 }
 
 func main() {
-	// initialize with no parent
-	head := Start(nil)
-	fmt.Println(head) // to see if it's working...
+	head := start(nil)
 
 	scanner := bufio.NewScanner(os.Stdin)
 	fmt.Print("> ")
 	for scanner.Scan() {
 		fmt.Print("> ")
 		input := strings.Split(scanner.Text(), " ")
+
 		if input[0] == "quit" {
 			break
-		} else {
-			switch strings.ToUpper(input[0]) {
-			case "START":
-				head = Start(head)
-				fmt.Println(head) //to see if it's working...
-			case "WRITE":
-				head.Write(input[1], input[2])
-			case "READ":
-				if val, err := head.Read(input[1]); err == nil {
-					fmt.Println(val)
-				} else {
-					fmt.Println(err)
-				}
-			case "DELETE":
-				head.Delete(input[1])
-			case "ABORT":
-				if isHead(head) == true {
-					fmt.Println("ERROR: ABORT called with no active transaction.")
-				} else {
-					head = Abort(head)
-					fmt.Println(head) // to see if it's working...
-				}
-			case "COMMIT":
-				head = Commit(head)
-				fmt.Println(head)
-			default:
-				fmt.Println("ERROR: Unknown command: " + input[0])
-			}
-
 		}
+
+		switch strings.ToUpper(input[0]) {
+		case "START":
+			head = start(head)
+		case "WRITE":
+			head.write(input[1], input[2])
+		case "READ":
+			if val, err := head.read(input[1]); err == nil {
+				fmt.Println(val)
+			} else {
+				fmt.Println(err)
+			}
+		case "DELETE":
+			head.delete(input[1])
+		case "ABORT":
+			if isTail(head) == true {
+				fmt.Println("ERROR: ABORT called with no active transaction.")
+			} else {
+				head = abort(head)
+			}
+		case "COMMIT":
+			head = commit(head)
+			fmt.Println(head)
+		default:
+			fmt.Println("ERROR: Unknown command: " + input[0])
+		}
+
 		if err := scanner.Err(); err != nil {
 			fmt.Fprintln(os.Stderr, "reading standard input:", err)
 		}
